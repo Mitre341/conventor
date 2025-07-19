44678ebe32100846a688d3bca311ded244a7bd5d
Mitre341 <emarkomitrovic@gmail.com>
2025-07-19 08:57:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function convert() {
 
     length.textContent = ` ${value} meters = ${(value * 3.28084).toFixed(3)} feet | ${value} feet = ${(value / 3.28084).toFixed(3)} meters `;// meters to feet
     volume.textContent = ` ${value} liters = ${(value * 0.264172).toFixed(3)} gallons | ${value} gallons = ${(value / 0.264172).toFixed(3)} liters `; // liters to gallons
-    mass.textContent = ` ${value} kilograms = ${(value * 2.20462).toFixed(3)} pounds |  ${value} pounds = ${(value * 2.20462).toFixed(3)} kilograms`; // kilograms to pounds
+    mass.textContent = ` ${value} kilograms = ${(value * 2.20462).toFixed(3)} pounds |  ${value} pounds = ${(value / 2.20462).toFixed(3)} kilograms`; // kilograms to pounds
 }
 
 button.addEventListener("click", convert);
